6aa5c5c60ff84a40ddc6514a3743c29971bbed3a
Tinus Nel <tinus.home@gmail.com>
2012-05-28 22:31:26 +0200

    Initial commit

---
+++ src/org/flying/lions/SMSReaderActivity.java
@@ -0,0 +1,14 @@
+package org.flying.lions;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class SMSReaderActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
